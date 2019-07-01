35f3b00291e3477da89c8cba1e60fd40474d983c
gabrielgsv <gabriel.soares.vilela@gmail.com>
2019-07-01 18:25:08 -0300

    feat: add material ui and your packages

---
+++ src/models/homeItem-models.ts
@@ -0,0 +1,17 @@
+export const homeItems: any[] = [
+  {name: "Para Família", image: "family"},
+  {name: "Eletricos", image: "electry"},
+  {name: "Esportivos", image: "sportive"},
+  {name: "Off-Road", image: "jeep"},
+]
+
+export const homeBrands: any[] = [
+  {image: "chevrolet"},
+  {image: "fiat"},
+  {image: "ford"},
+  {image: "vm"},
+  {image: "hyunday"},
+  {image: "renault"},
+  {image: "toyota"},
+  {image: "honda"},
+]
